146d46807e78bcf98e4053a34cb82c6a678c6111
anujbhandariNL <anuj@neuroleapmail.com>
2023-11-22 14:36:30 -0800

    Version one of generative stock advisor

--- src/App.js
+++ src/App.js
@@ -1,22 +1,138 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [recommendations, setRecommendations] = useState('');
+  const [hasClickedRecommend, setHasClickedRecommend] = useState(false);
+
+  const [marketSector, setMarketSector] = useState('Choose One');
+  const [holdingPeriod, setHoldingPeriod] = useState('Choose One');
+  const [maxPrice, setMaxPrice] = useState(500);
+  const [riskLevel, setRiskLevel] = useState('Choose One');
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/get-recommendations', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          marketSector: marketSector,
+          holdingPeriod: holdingPeriod,
+          maxPrice: maxPrice,
+          riskLevel: riskLevel
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setRecommendations(data.recommendations);
+        setHasClickedRecommend(true);
+      } else {
+        console.error('Failed to retrieve recommendations');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const renderRecommendations = () => {
+    if (recommendations) {
+      const paragraphs = recommendations.split('\n\n');
+      return paragraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ));
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className="app-logo"
+        src={require('./images/stock-exchange-market-with-chart-png.png')}
+        alt="Your Image Alt Text"
+      />
+        <h1>AI Stock Advisor</h1>
+        <form onSubmit={e => e.preventDefault()}></form>
+        <div>
+        <p>Market Sector</p>
+          <select
+            id="market-sector"
+            value={marketSector}
+            onChange={(e) => setMarketSector(e.target.value)}
+            className="custom-dropdown"
+          >
+            <option value="default">Choose One</option>
+            <option value="healthcare">Healthcare</option>
+            <option value="materials">Materials</option>
+            <option value="real-estate">Real Estate</option>
+            <option value="consumer-staples">Consumer Staples</option>
+            <option value="consumer-discretionary">Consumer Discretionary</option>
+            <option value="utilities">Utilities</option>
+            <option value="energy">Energy</option>
+            <option value="industrials">Industrials</option>
+            <option value="consumer-services">Consumer Services</option>
+            <option value="financials">Financials</option>
+            <option value="technology">Technology</option>
+          </select>
+        </div>
+        <div>
+          <p>Holding Period in Years</p>
+          <select
+            id="holding-period"
+            value={holdingPeriod}
+            onChange={(e) => setHoldingPeriod(e.target.value)}
+            className="custom-dropdown"
+          >
+            <option value="default">Choose One</option>
+            <option value="<1">Less than 1 year</option>
+            <option value="1">1 year</option>
+            <option value="2">2 years</option>
+            <option value="3">3 years</option>
+            <option value="4">4 years</option>
+            <option value="2">5 years</option>
+            <option value="3">6 years</option>
+            <option value="4">7 years</option>
+            <option value="2">8 years</option>
+            <option value="3">9 years</option>
+            <option value="4">10+ years</option>
+          </select>
+        </div>
+        <div>
+          <p>Risk Level</p>
+          <select
+            id="risk-level"
+            value={riskLevel}
+            onChange={(e) => setRiskLevel(e.target.value)}
+            className="custom-dropdown"
+          >
+            <option value="Choose">Choose One</option>
+            <option value="Low">Low</option>
+            <option value="Moderate">Moderate</option>
+            <option value="High">High</option>
+          </select>
+        </div>
+        <div>
+          <p>Max Price:</p>
+          <input
+            type="range"
+            id="max-price"
+            name="max-price"
+            min="1"
+            max="1000"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(parseInt(e.target.value, 10))}
+          />
+          <p>${maxPrice}</p>
+        </div>
+        <button className="submit-button" onClick={handleSubmit}>Get Recommendations</button>
+        {hasClickedRecommend && (
+          <div>
+            <h4>Your personalized recommendations:</h4>
+            <h6 className="output">{renderRecommendations()}</h6>
+          </div>
+        )}
       </header>
     </div>
   );
